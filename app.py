db3d9a34b18ef07e68024335d764360017fe1a18
33ben33 <111938822+33ben33@users.noreply.github.com>
2022-08-26 11:06:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+!pip install pyimgur
 import os
 import pandas as pd
 from datetime import datetime
@@ -5,6 +6,9 @@ import pandas as pd
 import pandas_datareader as pdr
 import datetime as datetime
 import matplotlib.pyplot as plt
+import mplfinance as mpf
+import yfinance as yf
+import pyimgur
 
 from flask import Flask, abort, request
 
@@ -34,15 +38,36 @@ def callback():
             abort(400)
 
         return "OK"
+IMGUR_CLIENT_ID ="98d4f054d5257ba"
+
+def plot_stcok_k_chart(IMGUR_CLIENT_ID,stock="0050" , date_from='2020-01-01' ):
+    """
+    進行個股K線繪製，回傳至於雲端圖床的連結。將顯示包含5MA、20MA及量價關係，起始預設自2020-01-01起迄昨日收盤價。
+    :stock :個股代碼(字串)，預設0050。
+    :date_from :起始日(字串)，格式為YYYY-MM-DD，預設自2020-01-01起。
+    """
+    stock = str(stock)+".tw"
+    # df = web.DataReader(stock, 'yahoo', date_from) 
+    df = yf.download(stock, date_from) 
+    mpf.plot(df,type='candle',mav=(5,20),volume=True, ylabel=stock.upper()+' Price' ,savefig='testsave.png')
+    PATH = "testsave.png"
+    im = pyimgur.Imgur(IMGUR_CLIENT_ID)
+    uploaded_image = im.upload_image(PATH, title=stock+" candlestick chart")
+    return uploaded_image.link
+
 
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    get_message = event.message.text
-    stock_number = '"event.message.text"+".TW"'
-    start = datetime.datetime(2022,7,1)
-    end   = datetime.datetime(2022,7,31)
-    pd= pdr.DataReader(stock_number, 'yahoo', start, end)
+        if event.message.text[:2].upper() == "股票":
+        input_word = event.message.text.replace(" ","") #合併字串取消空白
+        stock_name = input_word[2:6] #2330
+        start_date = input_word[6:] #2020-01-01
+        content = plot_stcok_k_chart(IMGUR_CLIENT_ID,stock_name,start_date)
+        message = ImageSendMessage(original_content_url=content,preview_image_url=content)
+        line_bot_api.reply_message(event.reply_token, message)
+    
+    
 
     # Send To Line
     reply = TextSendMessage(text=f"{get_message}")
